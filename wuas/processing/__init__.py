df097759fb5f422f626188241099d8758f5471fc
Silvio Mayolo <mercerenies@comcast.net>
2023-07-05 15:51:33 -0400

    Fix for processor registration

--- wuas/processing/__init__.py
+++ wuas/processing/__init__.py
@@ -5,6 +5,16 @@ creates output."""
 from .abc import BoardProcessor
 from .lighting import LightingProcessor
 
+import os
+import os.path
+import importlib
+
 __all__ = (
     'BoardProcessor', 'LightingProcessor',
 )
+
+# Load all modules in this directory, to allow processors to register
+# themselves. (TODO This excludes directories)
+_files = [f for f in os.listdir(os.path.dirname(__file__)) if f.endswith('.py') and f != '__init__.py']
+for current_file in _files:
+    importlib.import_module(__name__ + "." + current_file[:-3])
