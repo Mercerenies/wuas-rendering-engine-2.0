be9bc8572d9480e3279fb0b4a82bc44fdc43626b
Silvio Mayolo <mercerenies@comcast.net>
2024-11-24 11:16:30 -0500

    Refactor importing code into a helper function in util.py

--- wuas/processing/__init__.py
+++ wuas/processing/__init__.py
@@ -3,6 +3,7 @@
 creates output."""
 
 from .abc import BoardProcessor
+from wuas.util import import_immediate_submodules
 
 import os
 import os.path
@@ -14,14 +15,4 @@ __all__ = (
 
 # Load all modules in this directory, to allow processors to register
 # themselves.
-current_module_name = __name__.removesuffix('.__init__')
-for current_file in os.listdir(os.path.dirname(__file__)):
-    if current_file.endswith('.py') and current_file != '__init__':
-        module_name = current_module_name + "." + current_file[:-3]
-        importlib.import_module(module_name)
-    else:
-        full_path = os.path.join(os.path.dirname(__file__), current_file)
-        init_py = os.path.join(full_path, '__init__.py')
-        if os.path.isdir(full_path) and os.path.isfile(init_py):
-            module_name = current_module_name + "." + current_file
-            importlib.import_module(module_name)
+import_immediate_submodules(__file__, __name__)
--- wuas/util.py
+++ wuas/util.py
@@ -6,6 +6,9 @@ from __future__ import annotations
 from typing import Iterator, Any
 import math
 from pathlib import Path
+import importlib
+import os
+import os.path
 
 from PIL import ImageDraw
 
@@ -57,3 +60,31 @@ def walk_files_relative(root_dir: Path | str) -> Iterator[Path]:
     for dir, _, filenames in root_dir.walk():
         for filename in filenames:
             yield dir.relative_to(root_dir) / filename
+
+
+def import_immediate_submodules(
+    current_file: str,
+    current_module_name: str,
+    *,
+    include_folders: bool = True,
+) -> None:
+    """Imports all immediate submodules of the module given by the
+    argument file path and argument module name. The first two
+    arguments should generally be __file__ and __name__.
+
+    If include_folders is True (the default), then immediate
+    subfolders which contain __init__.py will also be imported. If
+    False, then only .py files in the current directory are imported.
+
+    """
+    current_module_name = current_module_name.removesuffix('__init__')
+    for file in os.listdir(os.path.dirname(current_file)):
+        if file.endswith('.py') and file != '__init__.py':
+            module_name = f'{current_module_name}.{file[:-3]}'
+            importlib.import_module(module_name)
+        elif include_folders:
+            full_path = os.path.join(os.path.dirname(current_file), file)
+            init_py = os.path.join(full_path, '__init__.py')
+            if os.path.isdir(full_path) and os.path.isfile(init_py):
+                module_name = f'{current_module_name}.{file}'
+                importlib.import_module(module_name)
