4bce66a7294ccf522f396ee3de75820804557282
Silvio Mayolo <mercerenies@comcast.net>
2024-11-24 11:58:11 -0500

    Extract registry API into a helper

--- wuas/processing/registry.py
+++ wuas/processing/registry.py
@@ -7,48 +7,8 @@ mapping."""
 from __future__ import annotations
 
 from wuas.processing.abc import BoardProcessor
+from wuas.util.registry import ClassRegistry
 
-from typing import Callable, Type, TypeVar, Any, overload
+REGISTERED_PROCESSORS = ClassRegistry[BoardProcessor]()
 
-_T_Type = TypeVar("_T_Type", bound=Type[BoardProcessor])
-
-REGISTERED_PROCESSORS: dict[str, Callable[[], BoardProcessor]] = {}
-
-
-@overload
-def registered_processor(cls: _T_Type) -> _T_Type: ...
-@overload
-def registered_processor(*, aliases: list[str]) -> Callable[[_T_Type], _T_Type]: ...
-
-
-def registered_processor(cls: Any = None, *, aliases: Any = None) -> Any:
-    """Decorator which registers a class as a processor. The class must
-    derive from BoardProcessor and must be constructible with no
-    arguments.
-
-    The class is registered with its own fully-qualified name (including
-    module). Optionally, aliases can be supplied, which will be
-    available as additional names for the class.
-
-    This decorator can be called with or without arguments. Calling the
-    decorator without arguments is equivalent to calling it with
-    aliases=[].
-
-    @registered_processor
-    class MyProcessor(BoardProcessor):
-        ...
-
-    @registered_processor(aliases=['myprocessor'])
-    class MyProcessor(BoardProcessor):
-        ...
-
-    """
-    if aliases is None:
-        return registered_processor(aliases=[])(cls)
-
-    def wrapper(cls: _T_Type) -> _T_Type:
-        REGISTERED_PROCESSORS[cls.__module__ + "." + cls.__name__] = cls
-        for alias in aliases:
-            REGISTERED_PROCESSORS[alias] = cls
-        return cls
-    return wrapper
+registered_processor = REGISTERED_PROCESSORS.register_class
---
+++ wuas/util/registry.py
@@ -0,0 +1,65 @@
+
+"""Helper utility for constructing global registries of particular
+classes."""
+
+from __future__ import annotations
+
+from typing import Mapping, Callable, Iterator, overload, Any
+
+
+class ClassRegistry[T](Mapping[str, Callable[[], T]]):
+    _dict: dict[str, Callable[[], T]]
+
+    def __init__(self) -> None:
+        self._dict = {}
+
+    def __getitem__(self, key: str) -> Callable[[], T]:
+        return self._dict[key]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._dict)
+
+    def __len__(self) -> int:
+        return len(self._dict)
+
+    def register_callable(self, name: str, func: Callable[[], T]) -> None:
+        if name in self._dict:
+            raise ValueError(f"Already registered: {name}")
+        self._dict[name] = func
+
+    @overload
+    def register_class[S: type[T]](self, cls: S) -> S: ...
+    @overload
+    def register_class[S: type[T]](self, *, aliases: list[str]) -> Callable[[S], S]: ...
+
+    def register_class(self, cls: Any = None, *, aliases: Any = None) -> Any:
+        """Decorator which registers a class with this registry. The
+        class must derive from the registry's target type T and must
+        be constructible with no arguments.
+
+        The class is registered with its own fully-qualified name
+        (including module). Optionally, aliases can be supplied, which
+        will be available as additional names for the class.
+
+        This decorator can be called with or without arguments.
+        Calling the decorator without arguments is equivalent to
+        calling it with aliases=[].
+
+        @registry.register_class
+        class MyClass:
+            ...
+
+        @registry.register_class(aliases=['myprocessor'])
+        class MyProcessor(BoardProcessor):
+            ...
+
+        """
+        if aliases is None:
+            return self.register_class(aliases=[])(cls)
+
+        def wrapper[S: type[T]](cls: S) -> S:
+            self._dict[f"{cls.__module__}.{cls.__name__}"] = cls
+            for alias in aliases:
+                self._dict[alias] = cls
+            return cls
+        return wrapper
